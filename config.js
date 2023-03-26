fd6cfe0ea53ac21bdb620e42dc690cb040d5d201
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-03-25 23:01:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  keywords: ["national geographic", "pass"],
-  comments: ["hello","let's be a friend","subs to subs ahaha"],
-  usernamegoogle: "usernamegoogle",
-  passwordgoogle: "your password",
-  delaycomment: 60, // delay ( per second )
+  keywords: ["national geographic","Kwazy6969kwazyiscoolunblocking", "KwazyCantTell"],
+  comments: ["Love it, actually great. Why dont this guy have more subs?","W Kwazy","L"],
+  usernamegoogle: "TIWBRAND@gmail.com",
+  passwordgoogle: "TIWMANAGEMENT6912",
+  delaycomment: 30, // delay ( per second )
   trending: true,//false
   copycomment:false, // false if you want use comment above , i mean from array
   userdatadir : "fdciabdul",
-  ai : false, // true if you want to use OPEN AI (CHAT-GPT) auto comment
-  apiKey : "YOUR OPENAI API KEY",
+  ai : true, // true if you want to use OPEN AI (CHAT-GPT) auto comment
+  apiKey : "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",
 };
